42361c96e7d13be65ad1cf4aca31063cb653503f
chrisfon <cfonsecar881@ulacit.ed.cr>
2023-05-05 16:11:23 -0600

    Added files

---
+++ CryptoPriceChecker/Model/CoinData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoPriceChecker.Model
+{
+    public class CoinData
+    {
+        public double usd { get; set; }
+        public double crc { get; set; }
+        
+        public CoinData() {
+           usd = 000;
+            crc = 000;
+            }
+
+    }
+
+   
+
+ 
+}
---
+++ CryptoPriceChecker/ViewModel/CoinViewModel.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Windows.Input;
+using CryptoPriceChecker.Model;
+
+
+
+namespace CryptoPriceChecker.ViewModel
+{
+    public class CoinViewModel : INotifyPropertyChanged
+    {
+
+        private string urlB;
+        private CoinData data;
+        private Boolean visible;
+        private string buscarUrl;
+
+        public Boolean isVisible 
+        { 
+            get { return visible; } 
+            set { visible = value; OnPropertyChanged(); }  
+        }
+        public string Url
+        {
+            get { return urlB; }
+            set { urlB = value; OnPropertyChanged(); }
+        }
+
+        public CoinData Data
+        {
+            
+            get => data;
+
+            set
+            {
+                data = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand buscarCommand { get; set; }
+
+        public CoinViewModel()
+        {
+            
+            buscarCommand = new Command(async (ticker) =>
+            {
+                var buscar = ticker as string;
+                var buscar2 = buscar.ToUpper();
+                buscarUrl = buscar2.ToLower();
+                urlB = $"https://assets.coincap.io/assets/icons/{buscarUrl}@2x.png";
+               // Url = url;
+                await GetData($"https://min-api.cryptocompare.com/data/price?fsym={buscar2}&tsyms=USD,CRC");
+
+            });
+
+        }
+
+        private async Task GetData(String url)
+        {
+            var cliente = new HttpClient();
+            var respuesta = await cliente.GetAsync(url);
+            respuesta.EnsureSuccessStatusCode();
+            
+            var jSonResult = await respuesta.Content.ReadAsStringAsync();
+            //data2 = JsonConvert.DeserializeObject<CoinData>(jSonResult).ToString();
+            var resultado = JsonConvert.DeserializeObject<CoinData>(jSonResult);
+
+           Data = resultado;
+            Url = urlB;
+            isVisible = true;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
